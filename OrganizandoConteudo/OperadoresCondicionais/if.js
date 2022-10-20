23d583808f0f3a5935e153530f1d4bc5112d85be
vitoria299 <92556441+vitoria299@users.noreply.github.com>
2022-10-20 12:26:51 -0300

    atualizacao do if

--- OrganizandoConteudo/OperadoresCondicionais/if.js
+++ OrganizandoConteudo/OperadoresCondicionais/if.js
@@ -1,10 +1,16 @@
-if(false) { //a condicao so funciona se for verdadeira(true)
-    console.log("testando o if")
+if(Condicao){ //a condicao so funciona se for verdadeira(true)
+    console.log("aqui é oque a condicao vai executar");
 }
 
-//criando um if
+//criando um if:
 var idade = 19;
+var idadeMinima = 18;
 
-if(idade > 18 ){
-    console.log("Pode fazer a carteira de habilitação");
+  if(idade > idadeMinima ){
+    console.log("Pode fazer a carteira de habilitação.");
 }
+    //é possivel criar um if seguido de outro:
+
+        if( idade > 15){
+            console.log("Precisa esperar 3 anos.");
+        }
