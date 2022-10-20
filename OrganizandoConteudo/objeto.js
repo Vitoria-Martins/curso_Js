937c782b475d77ffd6291f95c63df532cb598221
vitoria299 <92556441+vitoria299@users.noreply.github.com>
2022-10-19 23:31:48 -0300

    tudo sobre objetos

---
+++ OrganizandoConteudo/objeto.js
@@ -0,0 +1,25 @@
+//criando um objeto
+
+let obj = {
+    nome: "Vitoria",
+    idade: 19,
+    profissao: "Programadora",
+    estaTrabalhando: true,
+};
+
+//vendo objeto pelo terminal
+console.log(obj);
+
+//descobrindo qual tipo é
+console.log(typeof obj);
+
+// obj.idade chama o objeto idade
+console.log(obj.idade);
+
+//usando como variavel
+console.log("O meu nome é " + obj.nome);
+
+//mudando valor da propriedade
+obj.nome = "Veronica";
+console.log(obj.nome);
+
