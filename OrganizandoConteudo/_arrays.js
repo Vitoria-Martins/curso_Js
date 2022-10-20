c7e21434bbf7b0944b9acd5d0596fd32ac57811d
vitoria299 <92556441+vitoria299@users.noreply.github.com>
2022-10-19 23:57:23 -0300

    anotacoes sobre array

---
+++ OrganizandoConteudo/_arrays.js
@@ -0,0 +1,6 @@
+//array pode conter qualquer tipo de dado:
+var arr = [ "Vitoria", 19,  true, {objeto: 1}]; //objeto so recebe valor em numero, se naonda erro.
+console.log(arr);
+
+//acessando elemento
+console.log(arr[0])
