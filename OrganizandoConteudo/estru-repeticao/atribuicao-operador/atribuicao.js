f0ce086640e40bfea5fa331f6a5aba06f03476b7
vitoria299 <92556441+vitoria299@users.noreply.github.com>
2022-10-25 20:42:17 -0300

    atualicaçao atribuiçoes

---
+++ OrganizandoConteudo/estru-repeticao/atribuicao-operador/atribuicao.js
@@ -0,0 +1,20 @@
+var x = 1;
+var y = 2;
+
+// soma
+// x igual a x + y
+console.log(x = x + y);
+console.log(x += y);
+
+//subtracao
+console.log( x -= y);
+
+//multiplicaçao
+console.log( x *= y);
+
+//divisao
+console.log(x /=  y);
+
+//loops
+comsole.log(x++);
+console.log(X--);
