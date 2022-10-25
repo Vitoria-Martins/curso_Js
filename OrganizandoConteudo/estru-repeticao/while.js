9ca24f85e4eec6863b297729da7f135ee77565c6
vitoria299 <92556441+vitoria299@users.noreply.github.com>
2022-10-25 19:32:10 -0300

    atualizaçao while

---
+++ OrganizandoConteudo/estru-repeticao/while.js
@@ -0,0 +1,29 @@
+var x = 0;
+
+//enquanto x for menor que 5
+while( x < 5 ){
+    console.log("testando repetição" );
+
+    //incremento x++ serve para continuar o loop ate o valor final da condiçao (nesse caso o 5)
+    x++;
+}
+
+var arr = ['teste', 'testando', 'a', 'b'];
+
+var y = 0
+
+while( y <= 3){
+    //(arr[y] para passar por todos elementos do array)
+    console.log(arr[y]);
+    y++
+}
+
+//passa por todos carcteres de uma palavra
+var palavra = "Vitoria";
+var i = 0;
+
+while(i <= 6){
+    console.log(palavra[i]);
+
+    i++;
+}
