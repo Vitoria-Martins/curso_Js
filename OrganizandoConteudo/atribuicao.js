b2c9dce728129444cf5709b7f0107fd45d1bd9cd
vitoria299 <92556441+vitoria299@users.noreply.github.com>
2022-10-27 22:53:37 -0300

    aprendendo sobre for

---
+++ OrganizandoConteudo/atribuicao.js
@@ -0,0 +1,27 @@
+var x = 1;
+var y = 2;
+
+// soma
+// x igual a x + y
+console.log(x = x + y);
+console.log(x += y);
+
+//subtracao
+console.log( x -= y);
+
+//multiplicaçao
+console.log( x *= y);
+
+//divisao
+console.log(x /=  y);
+
+//loops
+comsole.log(x++);
+console.log(X--);
+
+
+while( x <= 100){
+    console.log(x);
+
+    X *= 2; 
+}
---
+++ OrganizandoConteudo/estru-repeticao/For.js
@@ -0,0 +1,17 @@
+// variavel ; condiçao ; incremento
+for(var i =0; i < 10; i++){
+     
+    console.log("Repetindo for: " + i );
+}
+
+var arr = ["Vitoria", "Martins" , 19 , "Brasileira"];
+
+
+// arr.length para retornar o comprimento do array. (se tiver 4 elementos, vai repetir 4 vezes)
+
+for(var v = 0; v < arr.length; v++) {
+
+    //arr[v] para exibir todo conteudo do array
+    console.log(arr[v]);
+
+}
