2be6a4cd4189c82bc2831f8fdb961f361337ab08
vitoria299 <92556441+vitoria299@users.noreply.github.com>
2022-12-05 21:21:18 -0300

    att

---
+++ OrganizandoConteudo/DOM/AdicionandoCSSnoDO/script.js
@@ -0,0 +1,9 @@
+var title = document.querySelector("#title");
+
+//adiciona o estilo
+title.style.color = "red";
+
+var subtitle = document.querySelector(".subtitle");
+
+//adiciona varios estilos
+subtitle.style.cssText = "color: blue; background-color: pink; font-size: 50px;";
