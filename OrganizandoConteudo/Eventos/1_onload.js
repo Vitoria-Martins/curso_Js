43fc917e74c9bd7bb5bb0b3ca5b769eb8a3f5f0e
vitoria299 <92556441+vitoria299@users.noreply.github.com>
2022-12-13 21:41:21 -0300

    event onload

--- OrganizandoConteudo/Eventos/1_onload.js
+++ OrganizandoConteudo/Eventos/1_onload.js
@@ -1,4 +1,15 @@
+// Onload So É executado depois do DOM 100% carregado
 window.onload = function(){
     console.log("Carregar o DOM");
-}// Onload So É executado depois do DOM
+
+
+    //encapsula a var dentro do onload para aparecer assim que o dom carregar 
+    var title2 = document.querySelector("#title");
+ console.log(title2);
+}
 console.log("Carregou o JS ");
+
+// pro evento ser carregado antes dos elementos da pgn, colocar a tag script acima de todos elementos como no CSS
+var title = document.querySelector("#title");
+
+console.log(title); 
